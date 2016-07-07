bef5907d3d6debc19ef5538a8265ac0a7935e707
Arnold Gandarillas Castillo <a.kltrx@gmail.com>
2016-07-07 15:46:25 -0400

    Add ListView to app

--- index.android.js
+++ index.android.js
@@ -7,6 +7,7 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  ListView,
   Text,
   View
 } from 'react-native';
@@ -14,13 +15,39 @@ import {
 import ActionButton from './components/ActionButton';
 import ListItem from './components/ListItems';
 import StatusBar from './components/StatusBar';
+import styles from './styles';
+
 
 class GroceryApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      })
+    };
+  }
+  componentDidMount() {
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows([{
+        title: 'Pizza'
+      }])
+    });
+  }
+  renderItem(item) {
+    return (
+      <ListItem item={item} onPress={() => {}}/>
+    );
+  }
   render() {
     return (
-      <View>
-        <ActionButton title="ReactNativeRocks" />
-        <StatusBar title="JS" />
+      <View style={styles.container}>
+        <StatusBar title="Grocery List" />
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this.renderItem.bind(this) }
+          style={styles.listview} />
+        <ActionButton title="Add" />
       </View>
     );
   }
