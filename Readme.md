c2e9118e05a198a33593fda6d0dca7e2c1952b48
Arnold Gandarillas Castillo <a.kltrx@gmail.com>
2016-07-07 12:46:09 -0400

    Add firebase bridge

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Grocery App
+
+*ReactNative, Firebase*
+
+Download *google-services.json* from firebase console and save it into android/app/
--- android/app/src/main/java/com/groceryapp/MainActivity.java
+++ android/app/src/main/java/com/groceryapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.groceryapp;
 
 import com.facebook.react.ReactActivity;
+import com.davecoates.rnfirebasebridge.FirebaseBridgePackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
@@ -34,7 +35,8 @@ public class MainActivity extends ReactActivity {
     @Override
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
-            new MainReactPackage()
+            new MainReactPackage(),
+            new FirebaseBridgePackage()
         );
     }
 }
